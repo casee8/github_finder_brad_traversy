c6c410abebda97cc7ea9b1e919395a4e0f3bc414
Zsolt Kaszas <pokerkaszy7@gmail.com>
2022-10-25 08:54:55 +0100

    Create navbar with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem'
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>test</h1>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <UserItem />
       </div>
     );
   }
