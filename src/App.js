109bfc9db10966268afd6b5b52df499b56a706b7
Zsolt Kaszas <pokerkaszy7@gmail.com>
2022-10-25 12:51:26 +0100

    Create UserItem with hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import Navbar from './components/layout/Navbar'
-import UserItem from './components/users/UserItem'
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
 class App extends Component {
+
   render() {
     return (
       <div className='App'>
