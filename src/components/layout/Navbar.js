686fa649e7dfdd3f29e27e5ded0649d45f6c2120
Zsolt Kaszas <pokerkaszy7@gmail.com>
2022-10-28 10:36:07 +0100

    Convert class-based components to functional

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,34 @@
-import React, { Component } from "react";
-import PropTypes from 'prop-types' //impt
+import React from "react";
+import PropTypes from "prop-types"; //impt
 
-class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github",
-  };
+const Navbar = ({icon, title}) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  }
+// static defaultProps = {
+//   title: "Github Finder",
+//   icon: "fab fa-github",
+// };
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon}></i> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
+// static propTypes = {
+//   title: PropTypes.string.isRequired,
+//   icon: PropTypes.string.isRequired,
+// }
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
 }
 
 export default Navbar;
