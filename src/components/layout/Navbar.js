8901767bf5366390d9d1165ff9872f0a13ce0796
Zsolt Kaszas <pokerkaszy7@gmail.com>
2022-11-04 16:47:23 +0000

    Create and separate Home.js from App.js + links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types"; //impt
+import PropTypes from "prop-types"; //impt <= shortcut
+import { Link } from "react-router-dom";
 
-const Navbar = ({icon, title}) => {
+const Navbar = ({ icon, title }) => {
   return (
     <nav className='navbar bg-primary'>
       <h1>
         <i className={icon}></i> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
@@ -29,6 +38,6 @@ Navbar.defaultProps = {
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
-}
+};
 
 export default Navbar;
